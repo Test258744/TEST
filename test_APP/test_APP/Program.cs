6865a3f2fc5ac82f5aa32817c553384f571f73b0
kirill <kirik756@yandex.ru>
2020-12-27 19:13:24 +0300

    Изменения

--- test_APP/test_APP/Program.cs
+++ test_APP/test_APP/Program.cs
@@ -6,6 +6,11 @@ namespace test_APP
     {
         static void Main(string[] args)
         {
+            //Изменения
+            //Изменения
+            //Изменения
+            //Изменения
+
             Console.WriteLine("TEST_APP_v.0.1.1");
             Console.WriteLine("Hello World!");
         }
