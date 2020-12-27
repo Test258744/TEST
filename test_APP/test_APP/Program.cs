708c1ad3f8077572898ca9c80e035a7f7b027238
kirill <kirik756@yandex.ru>
2020-12-27 19:05:07 +0300

    comit 2

---
+++ test_APP/test_APP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEST_APP_v.0.1.1");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
